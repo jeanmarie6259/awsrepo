8272a1c6ad27be8c11e9153c2ed2e008047ffb62
jeanmarie6259 <jeanmarie6259@wanadoo.fr>
2019-07-24 12:29:53 +0000

    configure api invokeURL 2

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://baezo16rhj.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://tbmwkq4dki.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
