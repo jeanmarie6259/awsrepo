826680d1d84be4aca9428c14f3b09e301849d7ec
jeanmarie6259 <jeanmarie6259@wanadoo.fr>
2019-07-24 09:22:39 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_qgdX6r2gP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2ea0r9134gu0lturdh76v2m9id', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
